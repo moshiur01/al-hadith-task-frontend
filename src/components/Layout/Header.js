41ca1ba21b57669925edf394629bfca4b23132ba
moshiur01 <mr.moshiur314@gmail.com>
2024-04-30 12:44:53 +0600

    search modal(initial design) added for sm and md devices

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import logo from "../../assets/asset 0.png";
 import helpingHand from "../../assets/helping-hand.svg";
 import drawerIcon from "../../assets/setting-drawer.svg";
+import SearchModal from "../UI/Modal/SearchModal";
 import RightSidebar from "../rightSide/RightSidebar";
 
 const Header = () => {
@@ -22,16 +23,50 @@ const Header = () => {
         </Link>
       </div>
 
-      <div className="navbar-end gap-16  mt-2 ">
+      <div className="navbar-end lg:gap-16 gap-5 mt-2 ">
         {/* search bar  for lg */}
-        <div className="relative flex font-sans ">
-          <div className="px-3 py-2 rounded-md flex items-center justify-center absolute top-1 left-1 bottom-1">
+        <div className="hidden lg:inline-block">
+          <div className="relative flex font-sans ">
+            <div className="px-3 py-2 rounded-md flex items-center justify-center absolute top-[6px] left-1 bottom-1 lg:size-10 ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+              >
+                <path
+                  d="M19 19L13 13L19 19ZM15 8C15 8.91925 14.8189 9.82951 14.4672 10.6788C14.1154 11.5281 13.5998 12.2997 12.9497 12.9497C12.2997 13.5998 11.5281 14.1154 10.6788 14.4672C9.82951 14.8189 8.91925 15 8 15C7.08075 15 6.1705 14.8189 5.32122 14.4672C4.47194 14.1154 3.70026 13.5998 3.05025 12.9497C2.40024 12.2997 1.88463 11.5281 1.53284 10.6788C1.18106 9.82951 1 8.91925 1 8C1 6.14348 1.7375 4.36301 3.05025 3.05025C4.36301 1.7375 6.14348 1 8 1C9.85652 1 11.637 1.7375 12.9497 3.05025C14.2625 4.36301 15 6.14348 15 8Z"
+                  stroke="#868686"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </div>
+            <input
+              className="w-full rounded-md pr-4 pl-12 py-3 shadow-sm focus:outline-none border-[1.5px] border-[#DEE1E5]  placeholder:text-mute-grey placeholder:text-base bg-white lg:w-64"
+              id="search"
+              name="search"
+              type="text"
+              placeholder="Search Hadith"
+            />
+          </div>
+        </div>
+
+        {/* search btn for sm and md*/}
+        <div>
+          <button className="bg-[#DEE1E5] size-11 flex justify-center items-center rounded-lg">
+            {/* search logo  */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="20"
               height="20"
               viewBox="0 0 20 20"
               fill="none"
+              onClick={() =>
+                document.getElementById("search-modal").showModal()
+              }
             >
               <path
                 d="M19 19L13 13L19 19ZM15 8C15 8.91925 14.8189 9.82951 14.4672 10.6788C14.1154 11.5281 13.5998 12.2997 12.9497 12.9497C12.2997 13.5998 11.5281 14.1154 10.6788 14.4672C9.82951 14.8189 8.91925 15 8 15C7.08075 15 6.1705 14.8189 5.32122 14.4672C4.47194 14.1154 3.70026 13.5998 3.05025 12.9497C2.40024 12.2997 1.88463 11.5281 1.53284 10.6788C1.18106 9.82951 1 8.91925 1 8C1 6.14348 1.7375 4.36301 3.05025 3.05025C4.36301 1.7375 6.14348 1 8 1C9.85652 1 11.637 1.7375 12.9497 3.05025C14.2625 4.36301 15 6.14348 15 8Z"
@@ -41,14 +76,8 @@ const Header = () => {
                 strokeLinejoin="round"
               />
             </svg>
-          </div>
-          <input
-            className="w-full rounded-md pr-4 pl-12 py-3 shadow-sm focus:outline-none border-[1.5px] border-[#DEE1E5]  placeholder:text-mute-grey placeholder:text-base bg-white"
-            id="search"
-            name="search"
-            type="text"
-            placeholder="Search Hadith"
-          />
+            <SearchModal id={"search-modal"} />
+          </button>
         </div>
 
         {/* donate btn */}
